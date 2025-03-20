4f83daac031337ea5808a8aacce4226c7650e4ce
Marc-Antoine ARNAUD <marc-antoine.arnaud@luminvent.com>
2025-03-20 07:17:52 +0100

    feat: display triples in sample

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,6 +3,8 @@ use codespan_reporting::files::SimpleFiles;
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 use std::fs::File;
 use std::io::Read;
+use rdf_types::generator::Blank;
+use rdf_types::Object;
 use turtle_syntax::{parsing::Parse, Document};
 
 fn main() -> std::io::Result<()> {
@@ -20,8 +22,19 @@ fn main() -> std::io::Result<()> {
 		let buffer = files.get(file_id).unwrap();
 
 		match Document::parse_str(buffer.source().as_str(), |span| span) {
-			Ok(_doc) => {
-				// do something
+			Ok(doc) => {
+				let document = doc.0;
+
+				let triples = document.build_triples(None, Blank::new()).unwrap();
+
+				for triple in triples {
+					let object = match triple.0.2.0 {
+						Object::Id(id) => id.as_str().to_string(),
+						Object::Literal(literal) => literal.value.0.as_str().to_string(),
+					};
+
+					println!("{} {} {}", triple.0.0.0.as_str(), triple.0.1.0.as_str(), object);
+				}
 			}
 			Err(error_and_span) => {
 				let e = error_and_span.0;
